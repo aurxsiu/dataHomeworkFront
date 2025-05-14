8582070ef20d8aaf38b3135c30b60f6a0fa8a124
aurxsiu <xiushou71@qq.com>
2025-05-15 07:12:45 +0800

    完成日记,多点导航和交通工具想摆烂了

--- src/js/mark_titles.js
+++ src/js/mark_titles.js
@@ -4,20 +4,76 @@ await loadComponent("/src/component/mark_component/mark_title_component.html");
 mark_title_component_init();
 
 const frame = document.getElementById("mark-titles-frame");
+const content_search_tip = document.getElementById("mark_titles_tip");
+let mapName = null;
+let filter = "";
 
-fetch("http://localhost:8080/map/getMarkTitles", {
-  method: "post",
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((response) => {
-    return response.json();
+let titles = [];
+
+setMapName(null);
+setTips([]);
+
+export function setTips(tips) {
+  if (tips.length != 0) {
+    let tipStr = "你可能想搜: ";
+    tips.forEach((item) => {
+      tipStr += '"' + item + '" ';
+    });
+    content_search_tip.innerHTML = tipStr;
+  } else {
+    content_search_tip.innerHTML = "";
+  }
+}
+
+export function setMapName(name) {
+  mapName = name;
+  fetch("http://localhost:8080/map/getMarkTitles", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      mapName: mapName,
+    }),
   })
-  .then((data) => {
-    data.forEach((item) => {
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      titles = data;
+      setFilter(filter);
+    });
+}
+
+export function setFilter(search_content) {
+  filter = search_content;
+  frame.innerHTML = "";
+  titles.forEach((item) => {
+    if (item.title.includes(filter)) {
       const markTitle = document.createElement("mark-title");
       frame.appendChild(markTitle);
       markTitle.init(item.title, item.rate, item.click);
-    });
+    }
   });
+  if (filter != "") {
+    filterInContent();
+  }
+}
+
+async function filterInContent() {
+  return await fetch("http://localhost:8080/map/filter", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      filter: filter,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setTips(data);
+    });
+}
